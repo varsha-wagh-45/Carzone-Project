145504e9d7b6814d5f279e85a97e5be828c41034
varsha-wagh-45 <varshawagh450@gmail.com>
2024-09-01 11:43:02 +0530

    completed teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db1',
+        'USER':'postgres',
+        'PASSWORD':'12345678',
+        'HOST':'localhost',
     }
 }
 
@@ -112,7 +115,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -123,3 +126,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'carzone/static'),
 ]
+
+#media settings
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL= '/media/'
